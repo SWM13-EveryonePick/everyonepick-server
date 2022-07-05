78244b831057cf10778ded212e781807e6120b4e
Young Hun Park <pyhpyh0670@gmail.com>
2022-07-06 01:19:26 +0900

    [PICK-26] ADD: 프로젝트 Init

---
+++ src/main/java/soma/everyonepick/EveryonepickApplication.java
@@ -0,0 +1,13 @@
+package soma.everyonepick;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EveryonepickApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EveryonepickApplication.class, args);
+	}
+
+}
---
+++ src/test/java/soma/everyonepick/EveryonepickApplicationTests.java
@@ -0,0 +1,13 @@
+package soma.everyonepick;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EveryonepickApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
