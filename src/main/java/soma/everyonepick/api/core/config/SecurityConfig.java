dc089c615112125f6f2e03fa23e33b0a756789ff
Young Hun Park <pyhpyh0670@gmail.com>
2022-08-01 13:49:25 +0900

    [PICK-52] FEAT: Security 관련 Config

---
+++ src/main/java/soma/everyonepick/api/core/config/SecurityConfig.java
@@ -0,0 +1,35 @@
+package soma.everyonepick.api.core.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+    public static final String[] EXCLUDED_URLS = {
+            "/api/test*",
+            "/api/user/refresh",
+            "/api/user/signup",
+    };
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .httpBasic().disable()
+                .csrf().disable()
+                .exceptionHandling()
+                .and()
+                .cors()
+                .and()
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+                .authorizeRequests()
+                .antMatchers(EXCLUDED_URLS).permitAll()
+                .anyRequest().authenticated();
+    }
+}
