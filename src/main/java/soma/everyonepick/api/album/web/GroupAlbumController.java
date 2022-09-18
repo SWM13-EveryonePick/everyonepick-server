3c7739621dc1ceb640a227e34d6ad4ed1298638c
Young Hun Park <pyhpyh0670@gmail.com>
2022-09-18 21:06:51 +0900

    [PICK-108] FIX: Swagger parameter content-type 수정

--- src/main/java/soma/everyonepick/api/album/web/GroupAlbumController.java
+++ src/main/java/soma/everyonepick/api/album/web/GroupAlbumController.java
@@ -2,10 +2,12 @@ package soma.everyonepick.api.album.web;
 
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
@@ -230,13 +232,16 @@ public class GroupAlbumController {
     @ApiResponses({
             @ApiResponse(responseCode = "200", description = "등록 성공")
     })
-    @PostMapping("/{groupAlbumId}/photo")
+    @PostMapping(
+            value = "/{groupAlbumId}/photo",
+            consumes = MediaType.MULTIPART_FORM_DATA_VALUE,
+            produces = MediaType.APPLICATION_JSON_VALUE
+    )
     public ResponseEntity<ApiResult<List<PhotoDto>>> uploadPhotos(
             @Parameter(description = "단체앨범 id", required = true)
             @PathVariable Long groupAlbumId,
             @Parameter(description = "업로드할 이미지 리스트")
-            @RequestParam(name = "images") List<MultipartFile> images
-
+            @RequestPart("images") List<MultipartFile> images
     ) {
         return ResponseEntity.ok(
                 ApiResult.ok(
