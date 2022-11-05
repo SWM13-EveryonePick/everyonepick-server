371b6f11487e2fdfc44136b3161eff021be5685c
Young Hun Park <pyhpyh0670@gmail.com>
2022-11-05 16:26:11 +0900

    [PICK-152] FIX: EventListener 수정

--- src/main/java/soma/everyonepick/api/album/event/FaceSwapRequestEventListener.java
+++ src/main/java/soma/everyonepick/api/album/event/FaceSwapRequestEventListener.java
@@ -2,6 +2,7 @@ package soma.everyonepick.api.album.event;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.event.EventListener;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.stereotype.Component;
@@ -20,7 +21,7 @@ public class FaceSwapRequestEventListener {
      * @param faceSwapRequestEvent 합성요청 이벤트
      */
     @Async
-    @TransactionalEventListener
+    @EventListener
     public void send(FaceSwapRequestEvent faceSwapRequestEvent) {
         faceSwapProducer.sendMessage(faceSwapRequestEvent.getFaceSwapRequestDto());
         log.info("message send");
