42fc879832db9c71e6f8ba61f4ac3b6068cfd312
Young Hun Park <pyhpyh0670@gmail.com>
2023-04-21 22:27:56 +0900

    [PICK-174] REFACT: FCM Credential 적용방법 변경.

--- src/main/java/soma/everyonepick/api/core/fcm/service/FirebaseCloudMessageService.java
+++ src/main/java/soma/everyonepick/api/core/fcm/service/FirebaseCloudMessageService.java
@@ -1,13 +1,11 @@
 package soma.everyonepick.api.core.fcm.service;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import com.google.auth.oauth2.GoogleCredentials;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
 import com.google.firebase.messaging.*;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import soma.everyonepick.api.album.entity.GroupAlbum;
 import soma.everyonepick.api.core.exception.EveryonepickException;
@@ -18,61 +16,23 @@ import soma.everyonepick.api.user.service.DeviceTokenService;
 import javax.annotation.PostConstruct;
 import java.io.*;
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 import java.util.stream.Collectors;
 
 @Slf4j
 @Service
 @RequiredArgsConstructor
 public class FirebaseCloudMessageService {
-    @Value(value = "${fcm.type}")
-    private String type;
-    @Value(value = "${fcm.project_id}")
-    private String project_id;
-    @Value(value = "${fcm.private_key_id}")
-    private String private_key_id;
-    @Value(value = "${fcm.private_key}")
-    private String private_key;
-    @Value(value = "${fcm.client_email}")
-    private String client_email;
-    @Value(value = "${fcm.client_id}")
-    private String client_id;
-    @Value(value = "${fcm.auth_uri}")
-    private String auth_uri;
-    @Value(value = "${fcm.token_uri}")
-    private String token_uri;
-    @Value(value = "${fcm.auth_provider_x509_cert_url}")
-    private String auth_provider_x509_cert_url;
-    @Value(value = "${fcm.client_x509_cert_url}")
-    private String client_x509_cert_url;
     private final DeviceTokenService deviceTokenService;
 
     @PostConstruct
     public void initFCM() {
-        ObjectMapper objectMapper = new ObjectMapper();
-        Map<String, String> map = new HashMap<>();
-
-        private_key = private_key.replace(" ", "\n");
-        private_key = "-----BEGIN PRIVATE KEY-----\n" + private_key + "\n-----END PRIVATE KEY-----\n";
-
-        map.put("type", type);
-        map.put("project_id", project_id);
-        map.put("private_key_id", private_key_id);
-        map.put("private_key", private_key);
-        map.put("client_email", client_email);
-        map.put("client_id", client_id);
-        map.put("auth_uri", auth_uri);
-        map.put("token_uri", token_uri);
-        map.put("auth_provider_x509_cert_url", auth_provider_x509_cert_url);
-        map.put("client_x509_cert_url", client_x509_cert_url);
-
         try {
-            String json = objectMapper.writeValueAsString(map);
+            FileInputStream serviceAccount =
+                    new FileInputStream("serviceAccountKey.json");
 
-            FirebaseOptions options = FirebaseOptions.builder()
-                    .setCredentials(GoogleCredentials.fromStream(new ByteArrayInputStream(json.getBytes())))
+            FirebaseOptions options = new FirebaseOptions.Builder()
+                    .setCredentials(GoogleCredentials.fromStream(serviceAccount))
                     .build();
 
             if (FirebaseApp.getApps().isEmpty()) {
